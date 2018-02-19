0d2d04b080496329bc928efb17bb83ff1cfd6ccb
Sunny Lam <sunny.lam@gmail.com>
2018-02-19 16:02:54 -0500

    added DL usrls

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
 	from distutils.core import setup
 
 config = {
-	'description': 'My Project',
-	'author': 'My Name',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
-	'author_email': 'My email',
+	'description': 'This program finds all files with a given prefix in a single folder and locates any gaps in numbering (i.e. spam001.txt, spam003.txt, missing spam002.txt)...  Also gives the option to insert gaps into numbered files so that a new file can be added.',
+	'author': 'Sunny Lam',
+	'url': 'https://github.com/sunnylam13/gap_fill_make_021918_1',
+	'download_url': 'https://github.com/sunnylam13/gap_fill_make_021918_1',
+	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
-	'packages': ['NAME'],
+	'packages': ['os, re, shutil'],
 	'scripts': [],
-	'name': 'projectname'
+	'name': 'Gap Filler Maker'
 }
 
 setup(**config)
