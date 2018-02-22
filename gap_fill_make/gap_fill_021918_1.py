890779e364119f1cbf2173126e984afeaade94fa
Sunny Lam <sunny.lam@gmail.com>
2018-02-22 15:46:05 -0500

    cleaned up comments

--- gap_fill_make/gap_fill_021918_1.py
+++ gap_fill_make/gap_fill_021918_1.py
@@ -223,12 +223,6 @@ def fileNum_changer(filename,current_filename_index,proc_file_list,regex,file_ol
 
 	try:
 
-		# go to file at position (new_num) in proc_file_list
-		# change current file `filename` so that its numbering matches `new_num`
-		# target_fileName = proc_file_list[new_num]
-
-		# need logic code here in case of `list index out of range` error
-
 		# change the filename's file_old_num to the new_number
 		# store it in altered_fileName
 
