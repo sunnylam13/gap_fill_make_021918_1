5e6b88d16852046ac78415a7171c5f8f3d59de09
Sunny Lam <sunny.lam@gmail.com>
2018-02-23 19:26:31 -0500

    revised code, realized I didn't need new_fileName_list, new_filePath_list = []

--- gap_fill_make/gap_make_021918_1.py
+++ gap_fill_make/gap_make_021918_1.py
@@ -33,6 +33,8 @@ user_input_folder = "../docs/testFolder2" # assign the folder to the input varia
 input_folder_dir = os.path.basename(user_input_folder)
 # print("The base name path to the folder is:  %s" % input_folder_dir)
 
+input_folder_filename_list = os.listdir(user_input_folder)
+
 # Get the number of files in the folder.
 true_max_num = len(os.listdir(user_input_folder)) # actual upper limit of numbering unlike highest_labelled_number()
 # print("The true_max_num is:  %i\n" % true_max_num)
@@ -113,6 +115,16 @@ user_cmnd_any_regex1 = re.compile(r'''
 # prefix_and_number_1 = gapFiller.prefix_regex2 # get it from imported file
 
 prefix_regex2 = re.compile(r'''
+		(?P<prefixLetters>^[a-z]+) # this is the group for the prefix, assumed to be a-z letters, one or more
+		(?P<leadZeroes>0*) # this is the the group for leading zeros, 0 or more i.e. 00 of 001
+		(?P<numbering>[1-9]*) # this is the group for the numbering
+		(?P<extension>(\..*$)) # this is the extension
+	''', re.VERBOSE)
+
+
+# need regex search that finds files you've now renamed "xxxx.ext-clone"
+# assumes you're working with ../docs/testFolder2/spam007.txt
+clone_regex1 = re.compile(r'''
 		(?P<prefixLetters>^[a-z]+) # this is the group for the prefix, assumed to be a-z letters, one or more
 		(?P<leadZeroes>0*) # this is the the group for leading zeros, 0 or more i.e. 00 of 001
 		(?P<numbering>[1-9]*) # this is the group for the numbering
@@ -389,12 +401,14 @@ def fileNum_changer(filename,current_filename_index,proc_file_list,regex,file_ol
 	else:
 		pass
 
-def rename_files(old_file_path,new_file_path):
+def rename_files(old_filenames,new_filenames,old_file_path,new_file_path):
 	# old_file_path and new_file_path are lists of paths not a single path
 	# use shutil.move to rename the file
 	# shutil.move(old_path,new_path)
 
 	regex_result,user_selected_cmd,user_selected_num = analyze_command(user_number_pos_input)
+	clone_filename_list = []
+	clone_path_list = []
 
 	try:
 		for item in old_file_path:
@@ -406,6 +420,10 @@ def rename_files(old_file_path,new_file_path):
 
 				if item == new_file_path[get_item_index]:
 					print("The old file path was not replaced:  %s" % (item))
+					clone_filename_list.append(new_filenames[get_item_index] + "-clone")
+					clone_path_list.append(new_file_path[get_item_index] + "-clone")
+					print("The file clone name is:  %s\n" % (new_filenames[get_item_index] + "-clone"))
+					print("The file clone_path is:  %s\n" % (new_file_path[get_item_index] + "-clone"))
 					pass
 				elif (item is not new_file_path[get_item_index]) and (get_item_index is not len(old_file_path)): # if the current file is not in the list of newly minted file paths and is not the last file to be processed
 					# print("The old file path replaced:  %s" % (item))
@@ -420,18 +438,32 @@ def rename_files(old_file_path,new_file_path):
 
 					# # place the copy back
 
-				print("The old file path replaced:  %s" % (item))
-				print("The new file path is:  %s" % (new_file_path[get_item_index]))
 
-				# the filename ahead already exists, the one before being renamed to match it overwrites leaving you with a gap
-				# store original filename
-				# create a copy of said original file path
-				prep_copyAhead_for_rename = new_file_path[get_item_index + 1]
-				print("The file prep_copyAhead_for_rename is:  %s" % prep_copyAhead_for_rename)
 
-				# shutil.move(item,new_file_path[get_item_index])
+					# the filename ahead already exists, the one before being renamed to match it overwrites leaving you with a gap
+					# create a copy of said original file name and file path
+					clone_filename_list.append(new_filenames[get_item_index + 1] + "-clone")
+					clone_path_list.append(new_file_path[get_item_index + 1] + "-clone")
+					print("The file clone name is:  %s\n" % (new_filenames[get_item_index + 1] + "-clone"))
+					print("The file clone_path is:  %s\n" % ((new_file_path[get_item_index + 1] + "-clone")) )
 
-				# place the copy back
+					# now rename the file to the number that's one ahead i.e. 006 becomes 007-copy
+					# shutil.move(item,new_file_path[get_item_index])
+					# shutil.move(clone_path,new_file_path[get_item_index])
+
+					# this means that by this point all the files you've now renamed are "xxxx.ext-clone"
+					# now you want to remove the "-clone" part
+					
+					# get the list of files in the folder, create another list of file paths to cycle through
+					new_fileName_list, new_filePath_list = []
+					new_filePath_list = fileTools.scanFile(user_input_folder,new_fileName_list) # since the function returns new_fileName_list
+
+					for item in new_fileName_list:
+						pass
+
+					print("The old file path replaced:  %s" % (item))
+					print("The new file path is:  %s" % (new_file_path[get_item_index]))
+
 				elif (get_item_index == len(old_file_path)): # if the current file is the last file
 					pass
 			elif user_selected_cmd == "before":
@@ -473,7 +505,7 @@ print(filePath_list_final)
 
 # original and new file names and paths have been generated
 	# all that remains is to execute the change
-rename_files(proc_filePath_list,filePath_list_final)
+rename_files(proc_file_list,file_list_final,proc_filePath_list,filePath_list_final)
 
 #####################################
 # END EXECUTION
